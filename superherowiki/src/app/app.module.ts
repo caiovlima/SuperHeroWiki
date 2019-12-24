b05ca114cd9e431d851595def5179338449517d2
Caio Vinicius De Lima <32105582+caiovlima@users.noreply.github.com>
2019-12-24 13:01:01 -0300

    Create Project And Make Base Layout

---
+++ superherowiki/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { MaterialModule } from './shared/material/material.module';
+import { HeroCardComponent } from './hero-card/hero-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeroCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ superherowiki/src/app/hero-card/hero-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hero-card',
+  templateUrl: './hero-card.component.html',
+  styleUrls: ['./hero-card.component.scss']
+})
+export class HeroCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
